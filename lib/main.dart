4df3cfca7cfb946547fe4e9bbeafc54182d39b49
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2023-05-23 14:01:31 -0600

    Add Firebase Connections

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout.dart';
@@ -5,7 +7,22 @@ import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 import 'package:instagram_flutter/utils/dimensions.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyCc-OfSUGOz17pfTHlGLJLGenH00UqOgGU',
+        appId: '1:207595377709:web:5d33f4991f8701dcfcd4a4',
+        messagingSenderId: '207595377709',
+        projectId: 'flutter-instagram-2d6b5',
+        storageBucket: 'flutter-instagram-2d6b5.appspot.com',
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
